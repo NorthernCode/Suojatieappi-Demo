28084777017e3d12e2626d393957b1cee00419b0
neppajymy <ilkka.sarkio@aalto.fi>
2015-11-13 13:05:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 alert("moi");
+
+// pikkuruutulista = tiles
+// isoruutulista = squares
+
+var tiles = {}{};
+var squares = {}{};
+tiles[5][5] = {};
+tiles[5][5].push([62.123,23.123]);
+alert(tiles[5][5]);
+
+function checkSquares(pos) {
+  
+}
