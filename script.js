4363a517b3ee0f3f99841b71fdb54cb0e5b95553
neppajymy <ilkka.sarkio@aalto.fi>
2015-11-14 00:35:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function checkTiles(gps) {
   var posy = gps.coords.latitude;
   var x = Math.floor(posx*d);
   var y = Math.floor(posy*d);
-  for (i = x-1; i < x+1; i++) { 
-    for (j = y-1; i < y+1; j++) {
+  for (j = y-1; i < y+1; j++) {
+    for (i = x-1; i < x+1; i++) {
       if (!(tiles[j])) {
         // ladataan isosta ruudusta dataa
         if (!loadSquares(i/d,j/d)) {
